1a3bdc4bff866f6fa73c55455e9554244bb455d6
MicroFish91 <mwfisher91@gmail.com>
2021-05-17 13:09:28 -0700

    Fixed event looping functionality

--- js/venuetest.js
+++ js/venuetest.js
@@ -25,13 +25,13 @@ let listGroupContainer = document.getElementById('temp')
 fetch(url)
 .then(response => response.json())
 .then(data =>{
-    console.log(data)
+    // console.log(data)
     //console.log(data.main.temp)
     //console.log(data._embedded)
     //console.log(data._embedded.venues[0].upcomingEvents.ticketmaster)
 
 
-    //let Venue = data._embedded.venues[0].upcomingEvents.ticketmaster
+    // let Venue = data._embedded.venues[0].upcomingEvents.ticketmaster
     let Venue = data._embedded.venues[0].id
 
     
@@ -43,19 +43,45 @@ fetch(url)
         // console.log(data._embedded.events[0].dates.start.localDate);
         // console.log(data._embedded.events[0].url);
     
-    
-    for(let i=0; i < data._embedded.events.length; i++){
+        console.log(data);
 
-        arr.push(
-        `<div>
-        ${data._embedded.events[i].name}
-        ${data._embedded.events[0].dates.start.localDate}
-        ${data._embedded.events[0].url}
-     
-        </div>`)
-    }
+        let event = {};
+
+        for(let i=0; i < data._embedded.events.length; i++){
+
+            // arr.push(data._embedded.events[i].name);
+
+            event.name = data._embedded.events[i].name;
+            event.startDate = data._embedded.events[i].dates.start.localDate;
+            event.url = data._embedded.events[i].url;
+            
+            arr.push(event);
+            event = {};
+        }
+
+        console.log(arr);
+        displayEvents(arr);
     })
     
 })
+
+}
+
+function displayEvents(eventsArray){
+    // purpose of function: loop through eventsArray and append events into HTML DOM
+
+    // querySelector
+
+    // you can either add events using the append method
+    // or
+    // you can add events in using innerHtml
+
+    let innerHtmlString = "";
+    // let location = document.querySelector('#somelocation')
+    
+    // for()
+    //     innerHtmlString += `<div>  </div>`
+
+    // location.innerHtml = innerHtmlString;
 
 }
